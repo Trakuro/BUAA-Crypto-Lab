91b82668fa22e477e1822cea492b0d4a4cdd3f32
Blackalas <trakuro@buaa.edu.cn>
2024-05-14 21:41:29 +0800

    finished Lab10. checked.

--- 10-Signature/ElGamal.py
+++ 10-Signature/ElGamal.py
@@ -7,12 +7,31 @@ hash_obj = sha256(m)
 hash = int.from_bytes(hash_obj.digest(), 'big')
 mode = input()
 
+def gcd(a, b):
+    while b != 0:
+        a, b = b, a % b
+    return a
+
+
+def extended_gcd(a, b):
+    if b == 0:
+        return 1, 0, a
+    else:
+        x, y, gcd = extended_gcd(b, a % b)
+        return y, x - (a // b) * y, gcd
+
+def mod_inverse(k, p):
+    assert gcd(k, p) == 1
+
+    x, _, _ = extended_gcd(k, p)
+    return x % p
+
 if mode == "Sign":
     x = int(input()) # Private Key
     k = int(input()) # Random Generated k
 
     r = pow(g, k, p)
-    s = (hash - x * r) * pow(k, p-1, p) % (p-1)
+    s = (hash - x * r) * mod_inverse(k, p-1) % (p-1)
 
     print(r, s)
 
