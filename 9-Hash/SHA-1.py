d86f83483c4ad39f4abb7e30c3e39f90bbe5365d
Blackalas <trakuro@buaa.edu.cn>
2024-05-14 17:42:14 +0000

    work on SHA-1

--- 9-Hash/SHA-1.py
+++ 9-Hash/SHA-1.py
@@ -1,5 +1,7 @@
 from typing import Union
 
+def left_rotate(value, bits):
+    return (value << bits) | (value >> (32 - bits)) & 0xffffffff
 
 def sha1(data: Union[bytes, bytearray]) -> bytes:
 
@@ -15,5 +17,60 @@ def sha1(data: Union[bytes, bytearray]) -> bytes:
     while len(data) % 64 != 56:
         data += b'\x00'
     data += len(data).to_bytes(8, byteorder="big")
+
+    # Break in to 64 byte chunks
+    chunks = []
+    while data:
+        chunks.append(data[:64])
+        data = data[64:]
+
+    # Process
+    digest = ""
+    
+    for chunk in chunks:
+        words = []
+        for i in range(16):
+            words.append(int.from_bytes(chunk[i*4:(i+1)*4], byteorder = "big"))
+
+        for i in range(16,80):
+            words.append(left_rotate(words[i-3] ^ words[i-8] ^ words[i-14] ^ words[i-16], 1))
+
+        # initial value for this chunk:
+        a, b, c, d, e = h0, h1, h2, h3, h4
+
+        for i in range(80):
+            if i in range(20):
+                f = (b & c) | ((~b) & d) 
+                k = 0x5a827999
+            elif i in range(20,40):
+                f = b ^ c ^ d
+                k = 0x6ed9eba1
+            elif i in range(40,60):
+                f = (b & c) | (b & d) | (c & d)
+                k = 0x8f1bbcdc
+            elif i in range(60,80):
+                f = b ^ c ^ d
+                k = 0xc162c1d6
+
+            temp = left_rotate(a, 5) + f + e + k + words[i]
+            e = d
+            d = c 
+            c = left_rotate(b, 30)
+            b = a
+            a = temp
+
+        h0 = ( a + h0 ) % 2 ** 32
+        h1 = ( b + h1 ) % 2 ** 3 
+        h2 = ( c + h2 ) % 2 ** 3 
+        h3 = ( d + h3 ) % 2 ** 3 
+        h4 = ( e + h4 ) % 2 ** 3 
+
+    digest = hex(h0)[2:] + hex(h1)[2:] + hex(h2)[2:] + hex(h3)[2:] + hex(h4)[2:]
+    return digest
+
+s = input()
+print(sha1(s.encode()))
+
+
 
     
